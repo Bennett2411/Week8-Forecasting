2708f95421c7cb9918abd08295bd54b444e0e2de
Bennett2411 <sharna.bennett@hotmail.com>
2025-03-06 08:37:01 +0000

    moved HTML to JS functions creating a loop

--- src/index.js
+++ src/index.js
@@ -55,4 +55,30 @@ function handleSearchSubmit(event) {
 let searchFormElement = document.querySelector('#searchform');
 searchFormElement.addEventListener('submit', handleSearchSubmit);
 
+function displayForecast() {
+  let forecastElement = document.querySelector('#forecast');
+
+  let days = [`Tue`, `Wed`, `Thu`, `Fri`, `Sat`];
+  let forecastHtml = '';
+
+  days.forEach(function (day) {
+    forecastHtml =
+      forecastHtml +
+      `
+      <div class="weather-forecast-day">
+            <div class="weather-forecast-date">${day}</div>
+            <div class="weather-forecast-icon">☁️</div>
+            <div class="weather-forcast-temperatures">
+              <div class="weather-forecast-temperature">
+                <strong>15°</strong>
+              </div>
+              <div class="weather-forecast-temperature">9°</div>
+            </div>
+          </div>
+          `;
+  });
+  forecast.innerHTML = forecastHtml;
+}
+
 searchCity('London');
+displayForecast();
